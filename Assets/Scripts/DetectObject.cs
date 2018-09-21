6f5701fbd6db2ac141ae4f6438c0bcd25e6725f1
ChaseTheAce <32078056+ChaseTheAce@users.noreply.github.com>
2018-09-20 21:01:50 -0500

    Lever Puzzle & Activatable Objects

---
+++ Assets/Scripts/DetectObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectObject : MonoBehaviour {
+
+    IActivatable activatableObject;
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    //We can also do a raycast
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ActivatableObject")
+        {
+            activatableObject = other.gameObject.GetComponent<IActivatable>();           
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        activatableObject = null;
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetButtonDown("Activate"))
+        {
+            if (activatableObject != null)
+            {
+                activatableObject.DoActivate();
+            }
+        }
+    }
+}
--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lever : MonoBehaviour {
+public class Lever : MonoBehaviour, IActivatable {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    string nameText;
+
+    [SerializeField]
+    LeverPuzzle leverPuzzle;
+
+    public string NameText
+    {
+        get
+        {
+            return nameText;
+        }
+    }
+
+    public void DoActivate()
+    {
+        // whatever we want to happen
+        leverPuzzle.CheckLever(this.gameObject);
+
+
+        Debug.Log("Lever Activated");  // just for testing
+    }
 }
