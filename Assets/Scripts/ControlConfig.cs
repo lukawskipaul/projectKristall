ae53bf358733b5014aa994f74267ab3722cc80fc
Paul Lukawski <Paul.Lukawski@loop.colum.edu>
2018-09-21 22:39:31 -0500

    GUI Edit, added Controls Reset function, added controls pop up to GUI, Cycling Control inputs

--- Assets/Scripts/ControlConfig.cs
+++ Assets/Scripts/ControlConfig.cs
@@ -8,20 +8,23 @@ public class ControlConfig : MonoBehaviour
 {
 
     bool isControllerConnected = false;
+    bool ShowPopup = false;
     public string Controller = "";
     public string PC_Move, PC_Rotate, PC_Item1, PC_Item2, PC_Item3, PC_Item4, PC_Inv, PC_Pause, PC_AttackUse, PC_Aim;
     public string Xbox_Move, Xbox_Rotate, Xbox_Item1, Xbox_Item2, Xbox_Item3, Xbox_Item4, Xbox_Inv, Xbox_Pause, Xbox_AttackUse, Xbox__Aim;
+    private KeyCode orig_pcItem1, orig_pcItem2, orig_pcItem3, orig_pcItem4, orig_pcInv, orig_pcPause, orig_xInv, orig_xPause;
     public ControlProfile cProfile;
     string ControlScheme;
     public KeyCode pcItem1, pcItem2, pcItem3, pcItem4, pcInv, pcPause, pcAttackUse, pcAim, xInv, xPause;
+    KeyCode PreviousKey;
 
     void DetectController()
     {
         try
         {
-            if(Input.GetJoystickNames()[0] != null)
+            if (Input.GetJoystickNames()[0] != null)
             {
-                isControllerConnected =  true;
+                isControllerConnected = true;
                 cProfile = ControlProfile.Controller;
                 IdentifyController();
             }
@@ -41,15 +44,22 @@ public class ControlConfig : MonoBehaviour
         Controller = Input.GetJoystickNames()[0];
     }
 
-    void SwitchProfile (ControlProfile Switcher)
+    void SwitchProfile(ControlProfile Switcher)
     {
         cProfile = Switcher;
     }
 
     void SetDefaultValues()
     {
-        if(!isControllerConnected)
+        if (!isControllerConnected)
         {
+            orig_pcItem1 = pcItem1;
+            orig_pcItem2 = pcItem2;
+            orig_pcItem3 = pcItem3;
+            orig_pcItem4 = pcItem4;
+            orig_pcInv = pcInv;
+            orig_pcPause = pcPause;
+
             PC_Move = "WASD";
             PC_Rotate = "Mouse";
             PC_Item1 = "1";
@@ -73,6 +83,15 @@ public class ControlConfig : MonoBehaviour
         }
         else
         {
+            orig_pcItem1 = pcItem1;
+            orig_pcItem2 = pcItem2;
+            orig_pcItem3 = pcItem3;
+            orig_pcItem4 = pcItem4;
+            orig_pcInv = pcInv;
+            orig_pcPause = pcPause;
+            orig_xInv = xInv;
+            orig_xPause = xPause;
+
             PC_Move = "WASD";
             PC_Rotate = "Mouse";
             PC_Item1 = "1";
@@ -111,85 +130,170 @@ public class ControlConfig : MonoBehaviour
 
     void OnGUI()
     {
-        GUI.BeginGroup(new Rect(Screen.width / 2 - 300, Screen.height / 2 - 300, 600, 400));
-        GUI.Box(new Rect(0, 0, 600, 400), "Controls");
-        GUI.Label(new Rect(205, 40, 20, 20), "PC");
-        GUI.Label(new Rect(340, 40, 125, 20), "Xbox Controller");
-
-        GUI.Label(new Rect(25,75,125,20), "Movement: ");
-        GUI.Button(new Rect(150,75,135,20), PC_Move);
-        GUI.Button(new Rect(325,75,135,20), Xbox_Move);
-
-        GUI.Label(new Rect(25, 100, 125, 20), "Rotation: ");
-        GUI.Button(new Rect(150, 100, 135, 20), PC_Rotate);
-        GUI.Button(new Rect(325, 100, 135, 20), Xbox_Rotate);
-
-        GUI.Label(new Rect(25, 125, 125, 20), "Item 1: ");
-        GUI.Button(new Rect(150, 125, 135, 20), PC_Item1);
-        GUI.Button(new Rect(325, 125, 135, 20), Xbox_Item1);
-
-        GUI.Label(new Rect(25, 150, 125, 20), "Item 2: ");
-        GUI.Button(new Rect(150, 150, 135, 20), PC_Item2);
-        GUI.Button(new Rect(325, 150, 135, 20), Xbox_Item2);
-
-        GUI.Label(new Rect(25, 175, 125, 20), "Item 3: ");
-        GUI.Button(new Rect(150, 175, 135, 20), PC_Item3);
-        GUI.Button(new Rect(325, 175, 135, 20), Xbox_Item3);
-
-        GUI.Label(new Rect(25, 200, 125, 20), "Item 4: ");
-        GUI.Button(new Rect(150, 200, 135, 20), PC_Item4);
-        GUI.Button(new Rect(325, 200, 135, 20), Xbox_Item4);
-
-        GUI.Label(new Rect(25, 225, 125, 20), "Inventory: ");
-        GUI.Button(new Rect(150, 225, 135, 20), PC_Inv);
-        GUI.Button(new Rect(325, 225, 135, 20), Xbox_Inv);
-
-        GUI.Label(new Rect(25, 250, 125, 20), "Pause Game: ");
-        GUI.Button(new Rect(150, 250, 135, 20), PC_Pause);
-        GUI.Button(new Rect(325, 250, 135, 20), Xbox_Pause);
-
-        GUI.Label(new Rect(25, 275, 125, 20), "attack/Use: ");
-        GUI.Button(new Rect(150, 275, 135, 20), PC_AttackUse);
-        GUI.Button(new Rect(325, 275, 135, 20), Xbox_AttackUse);
-
-        GUI.Label(new Rect(25, 300, 125, 20), "Aim: ");
-        GUI.Button(new Rect(150, 300, 135, 20), PC_Aim);
-        GUI.Button(new Rect(325, 300, 135, 20), Xbox__Aim);
-
-        GUI.Label(new Rect(450,345,125,20), "Current Controls");
-        if (GUI.Button(new Rect(425, 370, 135, 20), cProfile.ToString()))
+        if (!ShowPopup)
         {
-            if (cProfile == ControlProfile.Controller)
-                SwitchProfile(ControlProfile.PC);
-            else
-                SwitchProfile(ControlProfile.Controller);
-        }
+            GUI.BeginGroup(new Rect(Screen.width / 2 - 300, Screen.height / 2 - 300, 600, 400));
+            GUI.Box(new Rect(0, 0, 600, 400), "Controls");
+            GUI.Label(new Rect(205, 40, 20, 20), "PC");
+            GUI.Label(new Rect(340, 40, 125, 20), "Xbox Controller");
 
-        GUI.Label(new Rect(15,345,125,20), "Current Control Scheme");
-        if (GUI.Button(new Rect(25,370,135,20), ControlScheme))
-        {
-            if(ControlScheme == "Scheme A")
+            GUI.Label(new Rect(25, 75, 125, 20), "Movement: ");
+            GUI.Button(new Rect(150, 75, 135, 20), PC_Move);
+            GUI.Button(new Rect(325, 75, 135, 20), Xbox_Move);
+
+            GUI.Label(new Rect(25, 100, 125, 20), "Rotation: ");
+            GUI.Button(new Rect(150, 100, 135, 20), PC_Rotate);
+            GUI.Button(new Rect(325, 100, 135, 20), Xbox_Rotate);
+
+            GUI.Label(new Rect(25, 125, 125, 20), "Item 1: ");
+            if (GUI.Button(new Rect(150, 125, 135, 20), PC_Item1))
             {
-                SwitchControlScheme("B");
-                ControlScheme = "Scheme B";
+                ShowPopup = true;
+                PreviousKey = pcItem1;
             }
-            else
+            GUI.Button(new Rect(325, 125, 135, 20), Xbox_Item1);
+
+            GUI.Label(new Rect(25, 150, 125, 20), "Item 2: ");
+            if (GUI.Button(new Rect(150, 150, 135, 20), PC_Item2))
             {
-                SwitchControlScheme("A");
-                ControlScheme = "Scheme A";
+                ShowPopup = true;
+                PreviousKey = pcItem2;
             }
+            GUI.Button(new Rect(325, 150, 135, 20), Xbox_Item2);
+
+            GUI.Label(new Rect(25, 175, 125, 20), "Item 3: ");
+            if (GUI.Button(new Rect(150, 175, 135, 20), PC_Item3))
+            {
+                ShowPopup = true;
+                PreviousKey = pcItem3;
+            }
+            GUI.Button(new Rect(325, 175, 135, 20), Xbox_Item3);
+
+            GUI.Label(new Rect(25, 200, 125, 20), "Item 4: ");
+            if (GUI.Button(new Rect(150, 200, 135, 20), PC_Item4))
+            {
+                ShowPopup = true;
+                PreviousKey = pcItem4;
+            }
+            GUI.Button(new Rect(325, 200, 135, 20), Xbox_Item4);
+
+            GUI.Label(new Rect(25, 225, 125, 20), "Inventory: ");
+            if (GUI.Button(new Rect(150, 225, 135, 20), PC_Inv))
+            {
+                ShowPopup = true;
+                PreviousKey = pcInv;
+            }
+            if (GUI.Button(new Rect(325, 225, 135, 20), Xbox_Inv))
+            {
+                ShowPopup = true;
+                PreviousKey = xInv;
+            }
+
+            GUI.Label(new Rect(25, 250, 125, 20), "Pause Game: ");
+            if (GUI.Button(new Rect(150, 250, 135, 20), PC_Pause))
+            {
+                ShowPopup = true;
+                PreviousKey = pcPause;
+            }
+            if (GUI.Button(new Rect(325, 250, 135, 20), Xbox_Pause))
+            {
+                ShowPopup = true;
+                PreviousKey = xPause;
+            }
+
+            GUI.Label(new Rect(25, 275, 125, 20), "attack/Use: ");
+            GUI.Button(new Rect(150, 275, 135, 20), PC_AttackUse);
+            GUI.Button(new Rect(325, 275, 135, 20), Xbox_AttackUse);
+
+            GUI.Label(new Rect(25, 300, 125, 20), "Aim: ");
+            GUI.Button(new Rect(150, 300, 135, 20), PC_Aim);
+            GUI.Button(new Rect(325, 300, 135, 20), Xbox__Aim);
+
+            GUI.Label(new Rect(450, 345, 125, 20), "Current Controls");
+            if (GUI.Button(new Rect(425, 370, 135, 20), cProfile.ToString()))
+            {
+                if (cProfile == ControlProfile.Controller)
+                    SwitchProfile(ControlProfile.PC);
+                else
+                    SwitchProfile(ControlProfile.Controller);
+            }
+
+            GUI.Label(new Rect(15, 345, 125, 20), "Current Control Scheme");
+            if (GUI.Button(new Rect(25, 370, 135, 20), ControlScheme))
+            {
+                if (ControlScheme == "Scheme A")
+                {
+                    SwitchControlScheme("B");
+                    ControlScheme = "Scheme B";
+                }
+                else
+                {
+                    SwitchControlScheme("A");
+                    ControlScheme = "Scheme A";
+                }
+            }
+            if (GUI.Button(new Rect(230, 370, 135, 20), "Reset Controls"))
+            {
+                Reset();
+            }
+            GUI.EndGroup();
         }
-        GUI.EndGroup();
+        else
+        {
+            GUI.BeginGroup(new Rect(Screen.width / 2 - 300, Screen.height / 2 - 300, 600, 400));
+            GUI.Box(new Rect(0, 0, 600, 400), "Pick A Control to Switch");
+            if (GUI.Button(new Rect(150, 125, 135, 20), "1"))
+            {
+                SetNewKey(PreviousKey, orig_pcItem1);
+                ShowPopup = false;
+            }
+            if (GUI.Button(new Rect(150, 150, 135, 20), "2"))
+            {
+                SetNewKey(PreviousKey, orig_pcItem2);
+                ShowPopup = false;
+            }
+            if (GUI.Button(new Rect(150, 175, 135, 20), "3"))
+            {
+                SetNewKey(PreviousKey, orig_pcItem3);
+                ShowPopup = false;
+            }
+            if (GUI.Button(new Rect(150, 200, 135, 20), "4"))
+            {
+                SetNewKey(PreviousKey, orig_pcItem4);
+                ShowPopup = false;
+            }
+            if (GUI.Button(new Rect(150, 225, 135, 20), "I"))
+            {
+                SetNewKey(PreviousKey, orig_pcInv);
+                ShowPopup = false;
+            }
+            if (GUI.Button(new Rect(150, 250, 135, 20), "Escape"))
+            {
+                SetNewKey(PreviousKey, orig_pcPause);
+                ShowPopup = false;
+            }
+            if (GUI.Button(new Rect(325, 225, 135, 20), "A Button"))
+            {
+                SetNewKey(PreviousKey, orig_xInv);
+                ShowPopup = false;
+            }
+            if (GUI.Button(new Rect(325, 250, 135, 20), "Start Button"))
+            {
+                SetNewKey(PreviousKey, orig_xPause);
+                ShowPopup = false;
+            }
+            GUI.EndGroup();
+        }
     }
     void SwitchControlScheme(string Scheme)
     {
-        switch(Scheme)
+        switch (Scheme)
         {
             case "Scheme A":
                 SetDefaultValues();
                 break;
             case "Scheme B":
-                if(!isControllerConnected)
+                if (!isControllerConnected)
                 {
                     PC_Move = "WASD";
                     PC_Rotate = "Mouse";
@@ -249,5 +353,83 @@ public class ControlConfig : MonoBehaviour
                 }
                 break;
         }
+    }
+    void SetNewKey(KeyCode KeyToSet, KeyCode SetTo)
+    {
+        switch (KeyToSet)
+        {
+            case KeyCode.Alpha1:
+                pcItem1 = SetTo;
+                PC_Item1 = SetString(pcItem1.ToString());
+                break;
+            case KeyCode.Alpha2:
+                pcItem2 = SetTo;
+                PC_Item2 = SetString(pcItem2.ToString());
+                break;
+            case KeyCode.Alpha3:
+                pcItem3 SetTo;
+                PC_Item3 = SetString(pcItem3.ToString());
+                break;
+            case KeyCode.Alpha4:
+                pcItem4 = SetTo;
+                PC_Item4 = SetString(pcItem4.ToString());
+                break;
+            case KeyCode.I:
+                pcInv = SetTo;
+                PC_Inv = SetString(pcInv.ToString());
+                break;
+            case KeyCode.Escape:
+                pcPause = SetTo;
+                PC_Pause = SetString(pcPause.ToString());
+                break;
+            case KeyCode.JoystickButton1:
+                xInv = SetTo;
+                Xbox_Inv = SetString(xInv.ToString());
+                break;
+            case KeyCode.JoystickButton6:
+                xPause = SetTo;
+                Xbox_Pause = SetString(xPause.ToString());
+                break;
+        }
+    }
+    string SetString(string SetTo)
+    {
+        switch (SetTo)
+        {
+            case "Alpha 1":
+                SetTo = "1";
+                break;
+            case "Alpha 2":
+                SetTo = "2";
+                break;
+            case "Alpha 3":
+                SetTo = "3";
+                break;
+            case "Alpha 4":
+                SetTo = "4";
+                break;
+            case "Return":
+                SetTo = "Enter";
+                break;
+            case "Escape":
+                SetTo = "Escape";
+                break;
+            case "I":
+                SetTo = "I";
+                break;
+            case "JoystickButton6":
+                SetTo = "Start Button";
+                break;
+            case "JoystickButton1":
+                SetTo = "A Button";
+                break;
+        }
+        return SetTo;
+    }
+    void Reset()
+    {
+        SetDefaultValues();
+        ShowPopup = false;
+        PreviousKey = KeyCode.None;
     }
 }
