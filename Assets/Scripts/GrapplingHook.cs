fb1e8171af62defd0d4102baf4c212086f5b2bb3
Paul Lukawski <Paul.Lukawski@loop.colum.edu>
2018-10-15 10:21:11 -0500

    adding Grappling Hook and Hook Holder

---
+++ Assets/Scripts/GrapplingHook.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour
+{
+    public GameObject hook;
+    public GameObject hookHolder;
+
+    public float hookTravelSpeed;
+    public float playerTravelSpeed;
+    public float maxDistance;
+    private float currentDistance;
+
+    public static bool fired;
+    public static bool hooked;
+
+    private void Update()
+    {
+        //firing hook
+        if (InputManager.BButton() && fired == false)
+            fired = true;
+
+        if (fired == true)
+        {
+            hook.transform.Translate(Vector3.forward * Time.deltaTime * hookTravelSpeed);
+            currentDistance = Vector3.Distance(transform.position, hook.transform.position);
+
+            if (currentDistance >= maxDistance)
+                ReturnHook();
+        }
+    }
+
+    void ReturnHook()
+    {
+        hook.transform.position = hookHolder.transform.position;
+        fired = false;
+        hooked = false;
+    }
+}
+
